de51db532a5254651c54d0ce658e9973d6326845
michelelongo97 <m.longo7x@gmail.com>
2025-02-07 00:06:29 +0100

    fix: style

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,10 @@ export default function App() {
     event.preventDefault();
     alert("Inviato");
 
+    axios.post("http://localhost:3000/posts", postsList).then((response) => {
+      setPosts((currentPost) => [...currentPost, response.data]);
+    });
+
     setPosts((currentPost) => [...currentPost, postsList]);
 
     setPostsList({
@@ -44,41 +48,56 @@ export default function App() {
       <h1>Lista dei Post</h1>
       <ul>
         {posts.map((post) => (
-          <li key={post.title}>
-            {post.title} <hr /> {post.tags} <hr /> {post.content} <hr />{" "}
-            {post.image} <hr />
+          <li key={post.id}>
+            <h2>{post.title}</h2> <h5> {post.tags}</h5> <p>{post.content}</p>
+            <br /> <img src={post.image} alt={post.title} />
           </li>
         ))}
       </ul>
-      <form onSubmit={handleSubmit}>
-        <h3>Aggiungi un post</h3>
-        <input
-          type="text"
-          value={postsList.title}
-          onChange={(event) => handleField("title", event.target.value)}
-          placeholder="Scrivi l'articolo"
-        />
-        <input
-          type="text"
-          value={postsList.tags}
-          onChange={(event) => handleField("tags", event.target.value)}
-          placeholder="Scrivi dei tag"
-        />
-        <input
-          type="text"
-          value={postsList.content}
-          onChange={(event) => handleField("content", event.target.value)}
-          placeholder="Scrivi il contenuto"
-        />
-        <input
-          type="text"
-          value={postsList.image}
-          onChange={(event) => handleField("image", event.target.value)}
-          placeholder="Inserisci URL immagine"
-        />
-
-        <button type="submit">Invia</button>
-      </form>
+      <hr />
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <h3>Aggiungi un post</h3>
+          <div className="row">
+            <div>
+              <input
+                type="text"
+                value={postsList.title}
+                onChange={(event) => handleField("title", event.target.value)}
+                placeholder="Scrivi l'articolo"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                value={postsList.tags}
+                onChange={(event) => handleField("tags", event.target.value)}
+                placeholder="Scrivi dei tag"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                value={postsList.content}
+                onChange={(event) => handleField("content", event.target.value)}
+                placeholder="Scrivi il contenuto"
+                className="content"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                value={postsList.image}
+                onChange={(event) => handleField("image", event.target.value)}
+                placeholder="Inserisci URL immagine"
+              />
+            </div>
+            <div>
+              <button type="submit">Invia</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
